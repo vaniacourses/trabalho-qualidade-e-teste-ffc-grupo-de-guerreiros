5ffdf58312665116fbce3228c0ae378ddc1a0d39
Erivelton Campos <erivelton.de.campos@fazenda.rj.gov.br>
2026-04-20 14:00:06 -0300

    feat: teste de transferencia tipo: sucesso

--- src/test/java/TransferenciaTest.java
+++ src/test/java/TransferenciaTest.java
@@ -1,3 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.mycompany.a.transferencia;
+
 public class TransferenciaTest {
-    
+
+    @Test
+    public void testTransferenciaComSucesso() {
+        transferencia operacao = new transferencia();
+        String resultado = operacao.validarTransferencia(50.0, "C999", "C111", 100.0, true);
+        assertEquals("OK", resultado);
+    }
+
 }
