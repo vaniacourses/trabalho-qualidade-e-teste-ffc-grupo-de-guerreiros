7cc626e9d5e02a9aaf3c026824a5ffead8711f3c
Erivelton Campos <erivelton.de.campos@fazenda.rj.gov.br>
2026-04-20 14:01:07 -0300

    feat: teste saldo insuf e transferir mesma conta

--- src/test/java/TransferenciaTest.java
+++ src/test/java/TransferenciaTest.java
@@ -11,5 +11,19 @@ public class TransferenciaTest {
         String resultado = operacao.validarTransferencia(50.0, "C999", "C111", 100.0, true);
         assertEquals("OK", resultado);
     }
+     @Test
+    public void testTransferenciaSaldoInsuficiente() {
+        transferencia operacao = new transferencia();
+        String resultado = operacao.validarTransferencia(200.0, "C999", "C111", 100.0, true);
+        assertEquals("Saldo insuficiente.", resultado);
+    }
 
+    @Test
+    public void testTransferenciaMesmaConta() {
+        transferencia operacao = new transferencia();
+        String resultado = operacao.validarTransferencia(50.0, "C111", "C111", 100.0, true);
+        assertEquals("Não é possível transferir para a própria conta.", resultado);
+    }
+
+   
 }
