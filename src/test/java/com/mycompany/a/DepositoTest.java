bdb6a49122f22215a8d9c0b57d93b4f1aa12908b
Gleytton <gleytton@gmail.com>
2026-04-27 10:29:11 -0300

    chance DepositoTest.java

--- src/test/java/com/mycompany/a/DepositoTest.java
+++ src/test/java/com/mycompany/a/DepositoTest.java
@@ -1,28 +1,34 @@
 package com.mycompany.a;
 
 import org.junit.jupiter.api.Test;
-import com.mycompany.a.deposito;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
 
 
 public class DepositoTest {
-      @Test
+
+    deposito operacao;
+
+    @BeforeAll
+    public void iniciar(){
+        operacao = new deposito();
+    }
+
+    @Test
     public void testDepositoComSucesso() {
-        deposito operacao = new deposito();
         String resultado = operacao.validarDeposito(150.0);
         assertEquals("OK", resultado);
     }
 
     @Test
     public void testDepositoValorNegativo() {
-        deposito operacao = new deposito();
         String resultado = operacao.validarDeposito(-100.0);
         assertEquals("Valor inválido.", resultado);
     }
 
     @Test
     public void testDepositoValorZero() {
-        deposito operacao = new deposito();
         String resultado = operacao.validarDeposito(0.0);
         assertEquals("Valor inválido.", resultado);
     }
